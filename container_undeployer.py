4748c1d757a576fcb8fb84d79220f36ea23ed8fa
corka149 <corka149@mailbox.org>
2018-04-24 21:04:47 +0200

    Added multithreading capability

--- container_undeployer.py
+++ container_undeployer.py
@@ -1,13 +1,18 @@
 __author__ = "corka149"
 
+from pprint import pprint
+import concurrent.futures
 import xml.etree.ElementTree as ET
 import requests as r
 import misc
+import time
 
 misc.print_banner()
 
+auth = ("kieserver", "kieserver1!")
 kie_servers = ["localhost:8080"]
 selected = ""
+
 while not (selected.isdigit() and 0 <= int(selected) < len(kie_servers)) and selected != "q":
     print("Select one [q for quit]:")
     for k, v in enumerate(kie_servers):
@@ -20,44 +25,33 @@ if selected != "q":
     process_url = container_url + "/{}/processes"
     instance_url = process_url + "/instances"
 
-    user = "kieserver"
-    password = "kieserver1!"
-    auth = (user, password)
-
     container_results = r.get(container_url, auth=auth)
     root = ET.fromstring(container_results.text)
 
-    container_ids = list()
-    for container in root.iter("kie-container"):
-        container_ids.append(container.attrib["container-id"])
+    container_ids = [container.attrib["container-id"] for container in root.iter("kie-container")]
 
     selected = ""
-    should_quit = False
-    while not should_quit:
+    while not (selected.isdigit() and 0 <= int(selected) < len(container_ids)) and selected != "q":
         print("\nSelect one [q for quit]")
         for key, val in enumerate(container_ids):
             print("{}: {}".format(key, val))
         selected = input("Choose one: ")
-
-        if selected.isdigit() and 0 <= int(selected) < len(container_ids):
-            should_quit = True
-        elif selected == "q":
-            should_quit = True
 
     if selected != "q":
         selected_container = container_ids[int(selected)]
         definitions_result = r.get(instance_url.format(selected_container) + "?page=0&pageSize=100&sortOrder=true",
                                    auth=auth)
         instances = ET.fromstringlist(definitions_result.text)
+        to_delete = list()
+        start = time.time()
         for instance in instances.iter("process-instance"):
-            delete_url = instance_url.format(selected_container) + "/" + instance.find("process-instance-id").text
-            delete_result = r.delete(delete_url, auth=auth)
-            if delete_result.status_code == 204:
-                print("Delete process instance {} - {}".format(instance.find("process-instance-id").text,
-                                                               instance.find("process-name").text))
+            to_delete.append(instance_url.format(selected_container) + "/" + instance.find("process-instance-id").text)
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            result = tuple(executor.map(lambda url: r.delete(url, auth=auth), to_delete))
+            print("Amount of delete-requests: {}".format(len(result)))
+            pprint(result)
 
         container_results = r.delete(container_url + "/" + selected_container, auth=auth)
-        if container_results.status_code == 200:
-            print("Successfully disposed container {}".format(selected_container))
-        else:
-            print("Disposing of container {} failed.".format(selected_container))
+        print("Delete-request for container {} ends with {}.".format(selected_container, container_results.status_code))
+        print(f'\nTime to complete {time.time() - start:.2f}s\n')

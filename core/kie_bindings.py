db5a3786d14bed2769340ecbbd7da1caaf51f581
corka149 <corka149@mailbox.org>
2018-09-23 18:49:33 +0200

    Added kie bindings

---
+++ core/kie_bindings.py
@@ -0,0 +1,18 @@
+
+base_url = "http://{kie_server}kie-server/services/rest/server"
+
+
+def containers(kie_server):
+    return base_url.format(kie_server=kie_server) + "/containers"
+
+
+def process_instances(kie_server, container_id):
+    return containers(kie_server) + f"/{container_id}"
+
+
+def process_instances_variables(kie_server, container_id, process_id):
+    return process_instances(kie_server, container_id) + f"/processes/instances/{process_id}/variables"
+
+
+def process_instance(kie_server, container_id, process_id, with_vars):
+    return process_instances(kie_server, container_id) + f"/processes/instances/{process_id}?withVars={with_vars}"

d9945a71cd49f71f05a16e4577fefbb1033b78f2
corka149 <corka149@mailbox.org>
2018-10-04 22:01:19 +0200

    Added type hints and improved naming

--- core/__init__.py
+++ core/__init__.py
@@ -17,26 +17,26 @@ class KieClient:
         containers = container_results.json().get("result").get("kie-containers").get("kie-container")
         return [models.KieContainer(container) for container in containers]
 
-    def get_process_instances(self, container_id):
+    def get_process_instances(self, container_id: str):
         instances_url = kie_bindings.process_instances(self.__kie_server__, container_id)
         instances_result = self.__request_get__(instances_url)
         instances = instances_result.json().get("process-instance")
         return [models.ProcessInstance(instance) for instance in instances]
 
-    def get_process_variables(self, container_id, process_instance_id):
+    def get_process_variables(self, container_id: str, process_instance_id: str):
         url = kie_bindings.process_instances_variables(self.__kie_server__, container_id, process_instance_id)
         variables_result = self.__request_get__(url)
         return variables_result.json()
 
-    def delete_container(self, container_id):
+    def delete_container(self, container_id: str):
         url = kie_bindings.containers(container_id) + f"/{container_id}"
         return self.__request_delete__(url).status_code
 
-    def delete_process_instance(self, container_id, process_instance_id):
+    def delete_process_instance(self, container_id: str, process_instance_id: str):
         url = kie_bindings.process_instances(self.__kie_server__, container_id) + f"/{process_instance_id}"
         return self.__request_delete__(url).status_code
 
-    def start_process(self, container_id, process_definition_id, variable_dict=None):
+    def start_process(self, container_id: str, process_definition_id: str, variable_dict=None):
         """Starts a new process instance. It will return the status code of the request and the process id."""
         if variable_dict is None:
             variable_dict = dict()
@@ -44,30 +44,30 @@ class KieClient:
         resp = self.__request_post__(url, variable_dict)
         return resp.status_code, resp.json()
 
-    def get_task_by_instance(self, process_instance_id):
+    def get_tasks_by_instance(self, process_instance_id: str):
         url = kie_bindings.task_query(self.__kie_server__, process_instance_id)
         resp = self.__request_get__(url)
         return [models.Task(json) for json in resp.json().get("task-summary")]
 
-    def complete_task(self, container_id, task_id, variable_dict=None):
+    def complete_task(self, container_id: str, task_id: str, variable_dict=None):
         """Completes a task and return the response status code."""
         if variable_dict is None:
             variable_dict = dict()
         url = kie_bindings.task_state_complete(self.__kie_server__, container_id, task_id)
         return self.__request_put__(url, variable_dict).status_code
 
-    def __request_get__(self, url):
+    def __request_get__(self, url: str):
         logging.debug(f"GET - {url}")
         return requests.get(url, auth=self.__auth__, headers=self.__headers__)
 
-    def __request_delete__(self, url):
+    def __request_delete__(self, url: str):
         logging.debug(f"DELETE - {url}")
         return requests.delete(url, auth=self.__auth__)
 
-    def __request_post__(self, url, data):
+    def __request_post__(self, url: str, data: dict):
         logging.debug(f"POST - {url} - Body: {data}")
         return requests.post(url, json=data, auth=self.__auth__, headers=self.__headers__)
 
-    def __request_put__(self, url, data):
+    def __request_put__(self, url: str, data: dict):
         logging.debug(f"PUT- {url} - Body: {data}")
         return requests.put(url, json=data, auth=self.__auth__, headers=self.__headers__)
